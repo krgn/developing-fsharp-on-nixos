c639c90b036d6bc06634d4d37694c7c04f26da0f
karsten gebbert <k@ioctl.it>
2015-10-30 01:23:52 +0100

    most of parsers are done

--- sample/src/PaperScraper/Recoll.fs
+++ sample/src/PaperScraper/Recoll.fs
@@ -40,12 +40,17 @@ Common options:
 
 open FParsec
 
+let consume = true
+
 let plainChar (c : char) = 
   let chars = ['a'..'z']
   let pred c' = c' = c 
   match List.tryFind pred chars with
     | Some(_) -> true
     | _       -> false
+
+let skipLine : Parser<unit,unit> =
+  skipRestOfLine consume
 
 let plainChars : Parser<string, unit> = 
   manySatisfy plainChar
@@ -54,26 +59,118 @@ let mimeType : Parser<string, unit> =
   plainChars >>= fun res1 ->
     pstring "/" >>= fun _ ->
       plainChars >>= fun res2 ->
-        spaces >>= fun _ -> 
-          preturn (res1+"/"+res2)
+        preturn (res1+"/"+res2)
 
+let fileUrl : Parser<string,unit> =
+  pstring "file:///" >>= fun b ->
+    restOfLine consume >>= fun e -> preturn (b + e)
 
-let fileUrl : string = failwith "FIXME"
-let title : string = failwith "FIXME"
-let charSet : string = failwith "FIXME"
+let queryLine : Parser<string, unit> =
+  pstring "Recoll query:" >>. restOfLine consume
 
+let countLine : Parser<int64, unit> =
+  pint64 .>> skipRestOfLine consume
 
-          
-let test p str =
-  match run p str with
-    | Success(result, _, _) -> printfn "Success: %s" result
-    | Failure(msg, _, _)    -> printfn "Failure: %s" msg
+let inbrackets : Parser<string, unit> =
+  pstring "[" >>. manySatisfy ((<>) ']') .>> spaces
 
-let testInput =
+let abstractLine : Parser<string, unit> =
+  pstring "abstract = " >>. restOfLine consume
+
+let authorLine : Parser<unit, unit> =
+  attempt (pstring "author = " |>> ignore)
+
+let captionLine : Parser<unit, unit> =
+  attempt (pstring "caption = " |>> ignore)
+
+let titleLine : Parser<Name, unit> =
+  pstring "title = " >>. restOfLine consume
+
+let pcbytesLine : Parser<Bytes, unit> =
+  pstring "pcbytes = " >>. pint64 .>> skipRestOfLine consume
+
+let filenameLine : Parser<FileName, unit> =
+  pstring "filename = " >>. restOfLine consume
+  
+let mtypeLine : Parser<MimeType, unit> =
+  pstring "mtype = " >>. mimeType .>> skipRestOfLine consume
+
+let charsetLine : Parser<CharSet,unit> =
+  pstring "origcharset = " >>= fun str ->
+    preturn <| match str with
+                | "UTF-8" -> UTF8
+                | s       -> UnknownCharset s
+      .>> skipRestOfLine consume
+
+let relevance : Parser<Percentage,unit> =
+  pstring "relevancyrating = " >>. spaces >>. pint64 .>> skipRestOfLine consume
+  
+let urlLine : Parser<Url, unit> =
+  pstring "url = " >>. restOfLine consume
+
+let searchResult : Parser<SearchResult, unit> =
+  skipLine >>. 
+    abstractLine >>= fun abstrct ->
+      (authorLine <|> captionLine <|> skipLine) >>. skipLine >>.  // let's ignore all these
+        filenameLine >>= fun fname -> 
+          skipLine >>. skipLine >>.  // let's ignore these
+            mtypeLine >>= fun mime -> 
+              charsetLine >>= fun charset ->
+                pcbytesLine >>= fun size ->
+                  skipLine >>. 
+                    relevance >>= fun relv -> 
+                      skipLine >>. 
+                        titleLine >>= fun t -> 
+                          urlLine >>= fun url ->
+                            preturn { Abstract = abstrct
+                                    ; FileName = fname
+                                    ; MimeType = mime
+                                    ; CharSet = charset
+                                    ; Relevance = relv
+                                    ; Title = t
+                                    ; Url = url
+                                    ; FileSize = size 
+                                    }
+                              .>> skipRestOfLine consume
+      
+
+let bigOutput : Parser<BigQueryResult, unit> =
+  queryLine >>= fun query ->
+    countLine >>= fun count -> 
+      many searchResult >>= fun results ->
+        preturn { Query = query
+                ; Count = count
+                ; Rows  = results
+                }
+  
+let test = run (spaces >>. bigOutput)  
+
+let testInput1 =
+  @"
+Recoll query: ((monad:(wqf=11) OR monads OR monadic OR monadically OR monadicity OR monadized))
+1520 results (printing  1 max):
+text/plain	[file:///home/k/doc/nixconf/sample/src/PaperScraper/Recoll.fs]	[Recoll.fs]	4890	bytes	
+abstract = [<AutoOpen>] module PaperScraper.Recoll (* recollq: usage:  -P: Shw the date span for all the documents present in the index  [-o|-a|-f] [-q] <query string>  Runs a recoll query and displays result lines.    Default: will interpret the argument(s)
+dbytes = 4890
+fbytes = 4890
+filename = Recoll.fs
+fmtime = 01446155658
+mtime = 01446155658
+mtype = text/plain
+origcharset = UTF-8
+pcbytes = 4890
+rcludi = /home/k/doc/nixconf/sample/src/PaperScraper/Recoll.fs|
+relevancyrating = 100%
+sig = 48901446155658
+title = 
+url = file:///home/k/doc/nixconf/sample/src/PaperScraper/Recoll.fs
+  "
+
+let testInput2 =
   @"
 Recoll query: ((monad:(wqf=11) OR monads OR monadic OR monadically OR monadicity OR monadized))
 1519 results (printing  2 max):
-application/pdf	[file:///home/k/doc/books/cocharles-paper/A Poor Man's Concurrency Monad.pdf]	[A Poor Man's Concurrency Monad.pdf]	196433	bytes	
+application/pdf	[file:///home/k/doc/books/cocharles-paper/A Poor Man's Concurrency Monad.pdf]	[A Poor Man's Concurrency Monad.pdf]	196434	bytes	
 abstract =   c 1993 Cambridge University Press J. Functional Programming 1 (1): 1{000, January 1993  1 FUNCTIONAL PEARLS A Poor Man's Concurrency Monad Koen Claessen Chalmers University of Technology email: koen@cs.chalmers.se Abstract Without adding any
 dbytes = 21217
 fbytes = 196433
--- sample/src/PaperScraper/Types.fs
+++ sample/src/PaperScraper/Types.fs
@@ -4,15 +4,17 @@ module PaperScraper.Types
 type MimeType = string
 type FileName = string
 
+type Name = string
+
 type CharSet =
   | UTF8
-  | Other // how ignorant of me ;)
+  | UnknownCharset of string // how ignorant of me ;)
 
-type Percentage = int
-
+type Percentage = int64
+type QueryString = string
 type Url = string
 
-type Bytes = int
+type Bytes = int64
 
 type SearchResult =
   { Abstract  : string
@@ -20,8 +22,13 @@ type SearchResult =
   ; MimeType  : MimeType
   ; CharSet   : CharSet
   ; Relevance : Percentage
-  ; Title     : string option
+  ; Title     : Name
   ; Url       : Url
   ; FileSize  : Bytes
   }
 
+type BigQueryResult =
+  { Query : QueryString
+  ; Count : int64
+  ; Rows  : SearchResult list
+  }
